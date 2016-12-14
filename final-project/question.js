bbfc39e3b66fbad337d2c89b591fd9251369971f
joannabaranska <zinat.wakil@gmail.com>
2016-12-14 14:56:28 +0100

    Final project

---
+++ final-project/question.js
@@ -0,0 +1,106 @@
+var questions = [{ 
+	"question": "You are unexpectedly chosen for competing in Hogwart’s Triwizard Tournament. You feel you will have luck and eternal glory, but in fact you will be honoured only with one from the above.",
+	"option1": "Cedric Diggory",
+	"option2": "Draco Malfoy",
+	"option3": "Harry Potter",
+	"option4": "Terry Boot",
+	"answer": "1"
+}, {
+	"question": "A crystal ball is telling me, you will soon be flying on thestrals. You will try to save the one you love the most, but you will suffer afterwards.",
+	"option1": "Harry Potter",
+	"option2": "Cho Chang",
+	"option3": "Cornelius Fudge",
+	"option4": "Albus Dumbledore",
+	"answer": "1"
+}, {
+	"question": "I see this: the first year student’s kettle explodes during the classes. You will have to embarrassingly jump under the table.",
+	"option1": "Fred Weasley",
+	"option2": "Minerva McGonagall",
+	"option3": "Gregory Goyle",
+	"option4": "Severus Snape",
+	"answer": "4"
+}, {
+	"question": "Oh no, be careful! You will be trapped in Shrieking Shack with a serial murderer! But you will stay rational and solve part of the problem.",
+	"option1": "Luna Lovegood",
+	"option2": "Susan Bones",
+	"option3": "Hermione Granger",
+	"option4": "Katie Bell",
+	"answer": "3"
+}, {
+	"question": "The war started. One of the hands from your family clock will stop working soon.",
+	"option1": "Amos Diggory",
+	"option2": "Molly Weasley",
+	"option3": "Xenophilius Lovegood",
+	"option4": "Aurora Sinistra",
+	"answer": "2"
+}, {
+	"question": "You will have a baby who will master the art of camouflage.",
+	"option1": "Nymphadora Tonks",
+	"option2": "Narcissa Malfoy",
+	"option3": "Bellatrix Lestrange",
+	"option4": "Lily Potter",
+	"answer": "1"
+}, {
+	"question": "The useful gift from the Dark Lord will betray you and will be your end.",
+	"option1": "James Potter",
+	"option2": "Albus Dumbledore",
+	"option3": "Sirius Black",
+	"option4": "Peter Pettigrew",
+	"answer": "4"
+}, {
+	"question": "You will be an example of great power having a source in small person. You have to wait for your force to grow.",
+	"option1": "Percy Weasley",
+	"option2": "Bellatrix Lestrange",
+	"option3": "Ginny Weasley",
+	"option4": "Dolores Umbridge",
+	"answer": "3"
+}, {
+	"question": "Your particular proclivity for pyrotechnics will cause you a lot of problems, but also will help to save Hogwarts.",
+	"option1": "Hermione Granger",
+	"option2": "Theodore Nott",
+	"option3": "Seamus Finnigan",
+	"option4": "Luna Lovegood",
+	"answer": "3"
+}, {
+	"question": "One day you will be able to set off on a great trip. You will find many new magical beasts and one of the men who accompany you, will become your husband.",
+	"option1": "Parvati Patil",
+	"option2": "Luna Lovegood",
+	"option3": "Lavender Brown",
+	"option4": "Ginny Weasley",
+	"answer": "2"
+}, {
+	"question": "Oooh, there is a Grim at the bottom of my tea cup. You should be careful! Isn’t it the sign your uncle Bilius saw just before his tragic death?",
+	"option1":"Dean Thomas",
+	"option2": "Terry Boot",
+	"option3": "Angelina Johnson",
+	"option4": "Ron Weasley",
+	"answer": "4"
+}, {
+	"question": "Your inability to hold your tongue will help Voldemort to be more powerful and almost immortal. Almost.",
+	"option1": "Horace Slughorn",
+	"option2": "Filius Flitwick",
+	"option3": "Severus Snape",
+	"option4": "Albus Dumbledore",
+	"answer": "1"
+}, {
+	"question": "Do not bring home any unknown magical creatures hidden in the bag. Especially the ones you won during card game.",
+	"option1": "Professor Kettleburn",
+	"option2": "Firenzo",
+	"option3": "Rubeus Hagrid",
+	"option4": "Moaning Myrtle",
+	"answer": "3"
+}, {
+	"question": "The thing you relay on the most will not give you happiness. Your greatness doesn’t lie in bureaucracy.",
+	"option1": "Remus Lupin",
+	"option2": "Percy Weasley",
+	"option3": "Pansy Parkinson",
+	"option4": "Lee Jordan",
+	"answer": "2"
+}, {
+	"question": "Your sister forgave you. Do not pity the dead. Pity the living, and, above all those who live without love.",
+	"option1": "Ron Weasley",
+	"option2": "Tom Riddle",
+	"option3": "Albus Dumbledore",
+	"option4": "Sirius Black",
+	"answer": "3"
+}];
